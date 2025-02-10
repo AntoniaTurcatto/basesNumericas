36beda8632af25adee4a11caf549156244b39772
AntoniaTurcatto <lucasturcatto29@gmail.com>
2025-02-10 15:36:55 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main(){
+    int decimal = 15;
+    int octal = 017;
+    int hexadecimal = 0x0f;
+    int binario = 0b00001111;
+
+    std::cout << "n1 = " << decimal << std::endl;
+    std::cout << "n2 = " << octal << std::endl;
+    std::cout << "n3 = " << hexadecimal << std::endl;
+    std::cout << "n4 = " << binario << std::endl;
+}
